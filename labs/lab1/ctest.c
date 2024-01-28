5638682b2961defe47ef36212f011f2002c668a4
Mobina Tooranisama <mobinatoorani@gmail.com>
2024-01-28 01:09:57 -0500

    Add Labs

---
+++ labs/lab1/ctest.c
@@ -0,0 +1,15 @@
+/*
+ * ctest.c
+ *
+ * Created on: 2024-01-08
+ * Author: Mobina Tooranisama
+ */
+#include <stdio.h>
+
+int main() {
+  char letter;
+  for (letter = 'A'; letter <= 'z'; letter++) {
+    printf("%c\n", letter);
+  }
+  return 0;
+}
